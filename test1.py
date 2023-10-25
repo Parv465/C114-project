6f5449813bfe9d6d40e198a237b17d500446ce28
Parv465 <115660659+Parv465@users.noreply.github.com>
2023-10-25 19:51:16 +0100

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,8 @@
+a = 33
+b = "33"
+
+if b > a:
+    print("B is greater than A")
+elif a == b : 
+    print("A and B are equal")
+    # there is a type error due to B being a string and A being an int
---
+++ test2.py
@@ -0,0 +1,7 @@
+def my_function(fname):
+    print(fname + "REfsnes")
+
+my_function("Emil")
+my_function("Tobies")
+my_function("Linus")
+#the different names are then paired with "Refsnes"
